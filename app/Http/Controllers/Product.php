d6ba7a2ac87f0c921294e02b7b950b007074088b
kamendra singh gour <kamendrasau@gmail.com>
2021-03-02 18:07:55 +0530

    authorization

---
+++ app/Http/Controllers/Product.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Product extends Controller
+{
+    function index()
+    {
+        return "welcome to product page";
+    }
+}
